aaba6f7ee19961235041c21aebf3701f5fd93588
Pasak1095 <109857769+Pasak1095@users.noreply.github.com>
2022-08-26 18:16:09 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -118,8 +118,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
-			menuItem.x = 250;
+			menuItem.screenCenter(X);	
                         menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
@@ -274,8 +273,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-		  spr.screencenter(X);	
-		  spr.x = 250;
+		  spr.screencenter(X);
                 });
 	}
 
